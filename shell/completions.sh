7fc84906e53f8138cc2db4a02ec9a75a1cfbe050
Ingo Karkat <swdev@ingo-karkat.de>
2021-03-07 20:09:32 +0100

    completions: Add missing commands that also take SIMPLECOMMAND

--- shell/completions.sh
+++ shell/completions.sh
@@ -1,3 +1,3 @@
 #!/bin/sh source-this-script
 
-completeAsCommand eachArg eachFile exitsWith slowcommand succeedsOr withPath
+completeAsCommand eachArg eachFile exitsWith onfile processAddedFiles slowcommand succeedsOr tempdir withHome withPath withTempfile
